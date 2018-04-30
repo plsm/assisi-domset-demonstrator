54ee688b0690396222d31184b6e8b0f4ee3f0ee1
Pedro Lopes da Silva Mariano <plmariano@ciencias.ulisboa.pt>
2018-04-30 17:15:02 +0100

    First version of graph generator module.

---
+++ simulator/graph-generator.cpp
@@ -0,0 +1,32 @@
+/**
+ * Provides several functions to create diverse family of graphs.
+ *
+ * Graphs are saved in text files. The first line contains an integer, V,
+ * representing the number of vertices, and the second line contains an
+ * integer, E, representing the number of edges. Each of the following E lines
+ * contain two integers, a number between 1 and V, representing the vertices of
+ * an edge.
+ *
+ * N_V
+ * N_E
+ * e1v1 e1v2
+ * e2v1 e2v2
+ * ...
+ *
+ * @author Pedro Mariano
+ * @date 2018, April
+ */
+
+#include <stdio.h>
+
+void graph_n_m_star (FILE *fd, int n, int m)
+{
+	int V = n + m + 2;
+	int E = n + m + 1;
+	fprintf (fd, "%d\n%d\n", V, E);
+	for (int i = 0; i < n; i++)
+		fprintf (fd, "%d %d\n", i + 1, V - 1);
+	fprintf (fd, "%d %d\n", V, V - 1);
+	for (int i = 0; i < m; i++)
+		fprintf (fd, "%d %d\n", i + 1 + n, V);
+}
