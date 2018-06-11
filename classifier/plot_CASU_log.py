fe45a5be0db6793f81463ec4eb00c4cd9431bf5a
Pedro Lopes da Silva Mariano <plmariano@ciencias.ulisboa.pt>
2018-06-11 18:23:14 +0100

    Module plot_CASU_log uses module CASU_log

--- classifier/plot_CASU_log.py
+++ classifier/plot_CASU_log.py
@@ -1,43 +1,9 @@
 import argparse
-import csv
 import matplotlib
 import matplotlib.pyplot
-import os.path
 
 import assisipy.casu
-
-class CASU_log:
-    def __init__ (self, number, base_path = '.'):
-        def convert (a):
-            if a.find ('.') > -1:
-                return float (a)
-            else:
-                return int (a)
-        self.ir_raw = []
-        self.temp = []
-        self.led = []
-        with open (os.path.join (base_path, 'casu-{:03d}.csv'.format (number)), 'r') as fd:
-            reader = csv.reader (fd, delimiter=';', quoting = csv.QUOTE_MINIMAL)
-            for row in reader:
-                if row [0] == 'ir_raw':
-                    self.ir_raw.append ([convert(x) for x in row [1:]])
-                elif row [0] == 'temp':
-                    self.temp.append ([convert(x) for x in row [1:]])
-                elif row [0] == 'DiagnosticLed':
-                    self.led.append ([convert(x) for x in row [1:]])
-                #       Airflow
-            # DiagnosticLed
-            # dled_ref
-            # fft_amp
-            # fft_freq
-            # ir_range
-            # ir_raw
-            # Peltier
-            # Peltier_temp
-            # Speaker
-            # temp
-            # VibrationPattern
-            fd.close ()
+import CASU_log
 
 def process_arguments ():
     parser = argparse.ArgumentParser (
@@ -59,6 +25,7 @@ def process_arguments ():
     return parser.parse_args ()
 
 def plot (cl):
+    # type: (CASU_log.CASU_log) -> None
     figure = matplotlib.pyplot.figure ()
     number_axes = 3
     axes = [figure.add_axes ([0.05, i / float (number_axes) + 0.05, 0.9, (1.0 / number_axes - 0.05)]) for i in range (number_axes)]
@@ -81,7 +48,7 @@ def plot (cl):
 
 def main ():
     args = process_arguments ()
-    cl = CASU_log (args.casu, args.base_path)
+    cl = CASU_log.CASU_log (args.casu, args.base_path)
     plot (cl)
 
 main ()
